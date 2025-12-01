c5dd9a8da2320347c1aac39f793a03e4a0c013ab
chamardo <davidalgarcas@gmail.com>
2025-12-01 23:04:46 +0100

    Save and Load

--- MrHorseMan/src/Scene.cpp
+++ MrHorseMan/src/Scene.cpp
@@ -14,6 +14,7 @@
 #include "Enemy.h"
 #include "Item.h"
 
+
 Scene::Scene() : Module()
 {
 	name = "scene";
@@ -84,9 +85,113 @@ bool Scene::PreUpdate()
 
 bool Scene::Update(float dt)
 {
-	
-	return true;
+    auto input = Engine::GetInstance().input.get();
+
+    // -------- SAVE GAME (F5) ----------
+    if (input->GetKey(SDL_SCANCODE_F5) == KEY_DOWN)
+    {
+        LOG("Saving game...");
+        SaveGame();
+    }
+
+    // -------- LOAD GAME (F6) ----------
+    if (input->GetKey(SDL_SCANCODE_F6) == KEY_DOWN)
+    {
+        LOG("Loading game...");
+        LoadGame();
+    }
+
+    return true;
 }
+
+bool Scene::SaveGame()
+{
+    pugi::xml_document doc;
+    pugi::xml_node root = doc.append_child("game_state");
+
+    // Player
+    pugi::xml_node playerNode = root.append_child("player");
+    Vector2D pos = player->GetPosition();
+    playerNode.append_attribute("x") = pos.getX();
+    playerNode.append_attribute("y") = pos.getY();
+
+    // Enemies
+    pugi::xml_node enemiesNode = root.append_child("enemies");
+
+    int id = 0;
+    for (auto& e : Engine::GetInstance().entityManager->entities) // adjust to your container
+    {
+        if (e->type != EntityType::ENEMY) continue;
+
+        Enemy* enemy = dynamic_cast<Enemy*>(e.get());
+        if (!enemy) continue;
+
+        pugi::xml_node enemyNode = enemiesNode.append_child("enemy");
+        enemyNode.append_attribute("id") = id++;
+        enemyNode.append_attribute("x") = enemy->position.getX();
+        enemyNode.append_attribute("y") = enemy->position.getY();
+        enemyNode.append_attribute("dead") = enemy->pendingToDelete;
+    }
+
+    doc.save_file("savegame.xml");
+    LOG("Game saved!");
+    return true;
+}
+
+
+bool Scene::LoadGame()
+{
+    pugi::xml_document doc;
+    if (!doc.load_file("savegame.xml"))
+    {
+        LOG("Cannot load savegame.xml");
+        return false;
+    }
+
+    pugi::xml_node root = doc.child("game_state");
+    if (!root) return false;
+
+    // Player
+    pugi::xml_node playerNode = root.child("player");
+    if (playerNode)
+    {
+        float x = playerNode.attribute("x").as_float();
+        float y = playerNode.attribute("y").as_float();
+        player->SetPosition({ x, y });
+    }
+
+    // Enemies
+    pugi::xml_node enemiesNode = root.child("enemies");
+    if (enemiesNode)
+    {
+        auto entityIt = Engine::GetInstance().entityManager->entities.begin();
+        for (pugi::xml_node e = enemiesNode.child("enemy"); e && entityIt != Engine::GetInstance().entityManager->entities.end(); e = e.next_sibling("enemy"))
+        {
+            // Skip until we find the next enemy in the list
+            while (entityIt != Engine::GetInstance().entityManager->entities.end() && (*entityIt)->type != EntityType::ENEMY)
+            {
+                ++entityIt;
+            }
+            if (entityIt == Engine::GetInstance().entityManager->entities.end()) break;
+
+            Enemy* en = dynamic_cast<Enemy*>(entityIt->get());
+            if (!en) { ++entityIt; continue; }
+
+            en->position.setX(e.attribute("x").as_float());
+            en->position.setY(e.attribute("y").as_float());
+            en->pendingToDelete = e.attribute("dead").as_bool();
+
+            ++entityIt; // move to the next entity
+        }
+    }
+
+    LOG("Game loaded!");
+    return true;
+}
+
+
+
+
 
 // Called each loop iteration
 bool Scene::PostUpdate()
